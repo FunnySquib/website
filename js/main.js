4aa70a447dab22a77b45e39ae9de81e41a1443c9
joey <mac.chips89@gmail.com>
2024-11-11 13:55:28 -0600

    fixing glass city

--- js/main.js
+++ js/main.js
@@ -1,58 +1,41 @@
-// Hello there!
-//
-// If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#1791
-
 console.warn(
-  "%cNote!",
-  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io\nPlease do not just add them without asking me first! Thank you!"
+  "%cHello!!",
+  "color: red; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
+  "Feel free to use anything you find here for your projects; credit is appreciated but not required! Visit my website at https://3kh0.net for more information."
 );
 
-function script(text) {
-  console.log("%cScript Injection", "color: cyan; font-weight: 600; background: black; padding: 0 5px; border-radius: 5px", text);
+// this setting controls if ads are shown,
+// more info on the README.md file
+var adStatus = localStorage.getItem("adConsent") === "true"; // default: true
+
+if (!adStatus) {
+  (function () {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5756835229788588";
+    script.crossOrigin = "anonymous";
+    document.head.appendChild(script);
+    console.log("Ads enabled, thank you for your support!");
+  })();
 }
 
-// ====================================
-// SCRIPT INJECTION
-// ====================================
+const script = document.createElement("script");
+script.src = "https://data.3kh0.net/script.js";
+script.defer = true;
+script.setAttribute("data-website-id", "47d72bde-ba44-4125-b161-00e0c2f5b7f0");
+document.head.appendChild(script);
+script.onload = function() {
+  console.log("Data script loaded");
+  umami.track([location.hostname, "pageview"].join("/"));
+};
 
-var gaenabled = window.localStorage.getItem("ga");
-if (gaenabled == "false") {
-  script("Skipped GA injection because it is disabled by the user.");
-} else {
-  const gascript = document.createElement("script");
-  gascript.setAttribute("async", "");
-  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
-  const inlinegascript = document.createElement("script");
-  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-98DP5VKS42');`;
-  document.head.append(gascript, inlinegascript);
-  script("Injected script 1/5");
+const local_title = localStorage.getItem("title");
+const local_icon = localStorage.getItem("icon");
+if (window.localStorage.hasOwnProperty("title")) {
+  document.title = local_title;
+  console.log("Title set to: " + local_title);
 }
-
-const tabCloak = document.createElement("script");
-tabCloak.setAttribute("src", "/js/tab_cloak.js");
-document.head.append(tabCloak);
-script("Injected script 2/5");
-
-const arc = document.createElement("script");
-arc.setAttribute("async", "");
-arc.setAttribute("src", "https://arc.io/widget.min.js#eRPHFgiC");
-document.head.append(arc);
-script("Injected script 3/5");
-
-const adblock = document.createElement("script");
-adblock.setAttribute("src", "https://fundingchoicesmessages.google.com/i/pub-5756835229788588?ers=1");
-adblock.setAttribute("nonce", "yibq-w_TR5NOCRWsU-VL0Q");
-adblock.setAttribute("async", "");
-document.head.append(adblock);
-script("Injected script 4/5");
-
-const ad = document.createElement("script");
-ad.setAttribute("async", "");
-ad.setAttribute("src", "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5756835229788588");
-ad.setAttribute("crossorigin", "anonymous");
-document.head.append(ad);
-script("Injected script 3/5");
+if (window.localStorage.hasOwnProperty("icon")) {
+  document.querySelector("link[rel=icon]").href = local_icon;
+  console.log("Icon set to: " + local_icon);
+}
